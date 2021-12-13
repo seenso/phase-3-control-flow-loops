eff35581859b9f1230e0b9b5488e680c07eed0c0
seenso <>
2021-12-12 18:57:33 -0900

    commit tree 466c8c2955ac1f00be369be4f399f1909edf9d06

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
 def happy_new_year
   # your code here
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+    if i == 0
+      puts "Happy New Year!"
+    end
+  end
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +25,22 @@ end
 
 def fizzbuzz_printer
   # your code here
+  i = 1
+  while i <= 100
+    if fizzbuzz(i) != i
+      puts fizzbuzz(i)
+    else
+      puts i
+    end
+    i += 1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  # https://medium.com/@pinglinh/how-to-reverse-a-string-in-ruby-d8e53d31065d
+  strArr = str.split("");
+  reversed = []
+  str.size.times { reversed << strArr.pop} # << is the same as .push() in JS
+  reversed.join
 end
